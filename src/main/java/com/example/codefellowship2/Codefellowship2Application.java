5b94f3f29ce1ea219cf92cdfa07bfcf7b51e5dcb
Majd Khasawneh <majd.motaz@yahoo.com>
2021-08-16 16:31:46 +0300

    kms

---
+++ src/main/java/com/example/codefellowship2/Codefellowship2Application.java
@@ -0,0 +1,13 @@
+package com.example.codefellowship2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codefellowship2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Codefellowship2Application.class, args);
+	}
+
+}
